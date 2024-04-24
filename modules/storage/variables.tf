3a464ddd03bab3d49417936d194f24007e073c24
Ramesh Pentakota <ramesh.pch@gmail.com>
2024-04-24 01:07:14 -0700

    Update variables.tf

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,5 +1,14 @@
-provider "google" {
-  project = "PROJECT ID"
-  region  = "REGION"
-  zone    = "ZONE"
+variable "project_id" {
+  description = "The project ID"
+  default     = "PROJECT ID HERE"
+}
+
+variable "region" {
+  description = "region"
+  default     = "region HERE"
+}
+
+variable "zone" {
+  description = "zone"
+  default     = "zone HERE"
 }
