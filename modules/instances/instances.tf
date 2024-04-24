8098d49056e39c1daa02bb4cfecf755dc6260eca
Chinni Ramesh Pentakota <ramesh.pch@gmail.com>
2024-04-24 02:47:43 -0700

    Update instances.tf

--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -1,43 +1,33 @@
 resource "google_compute_instance" "tf-instance-1" {
   name         = "tf-instance-1"
   machine_type = "e2-micro"
-  zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "tf-instance-1"
-      }
     }
   }
 
   network_interface {
-    network = "default"
+ network = "default"
   }
 
-  allow_stopping_for_update = true
-
+allow_stopping_for_update = true
 }
 
 resource "google_compute_instance" "tf-instance-2" {
   name         = "tf-instance-2"
   machine_type = "e2-micro"
-  zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "tf-instance-2"
-      }
     }
   }
 
   network_interface {
-    network = "default"
+ network = "default"
   }
 
-  allow_stopping_for_update = true
-
+allow_stopping_for_update = true
 }
